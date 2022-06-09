08ecc6385c4364fb22b45ef6e300d3a6e4c05860
DimaPashchenko <kitaminchik@gmail.com>
2022-06-09 16:42:46 +0300

    Change contacts/new -> /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  resource :contacts, only: [:new, :create]
+  #get 'contacts' => 'contacts#new' - коли зі строки знизу видаляєто :new
+  resource :contacts, only: [:new, :create], path_names: {:new => ''}
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
