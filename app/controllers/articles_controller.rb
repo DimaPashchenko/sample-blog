338d4faed207735290677f7f1d9853d6bb135af4
DimaPashchenko <kitaminchik@gmail.com>
2022-06-12 16:56:15 +0300

    Implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,21 @@
 class ArticlesController < ApplicationController
 
 	def new
-		@article = Article.new
 	end
 
 	def create
-		render plain: params[:article].inspect # plain - ключ хеша, парамс...-значення хеша
+		@article = Article.new(article_params)
+		if @article.valid?
+		   @article.save
+		else
+		   render action: 'new'
+		end	
+	end
+
+	private
+	
+	def article_params
+		params.require(:article).permit(:title, :text)
 	end
 
 end
