402361c1bd7627cd18012764d23d1cd4a55f5756
DimaPashchenko <kitaminchik@gmail.com>
2022-06-12 17:14:33 +0300

    Update articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,14 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		if @article.valid?
 		   @article.save
+		   redirect_to @article
 		else
 		   render action: 'new'
 		end	
 	end
 
 	private
-	
+
 	def article_params
 		params.require(:article).permit(:title, :text)
 	end
