223de7284cbb70b509852bea155625da05127899
DimaPashchenko <kitaminchik@gmail.com>
2022-06-28 17:37:50 +0300

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   #get 'contacts' => 'contacts#new' - коли зі строки знизу видаляєто :new
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
